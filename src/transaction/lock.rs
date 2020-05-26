e0641dbb28734ac81355f8040ad8e543e178bb98
Renkai <gaelookair@gmail.com>
2020-05-26 12:48:54 +0800

    reduce unnecessary clone
    Signed-off-by: Renkai <gaelookair@gmail.com>

--- src/transaction/lock.rs
+++ src/transaction/lock.rs
@@ -56,13 +56,14 @@ pub async fn resolve_locks(
         .collect();
 
     for ((_region_ver_id, lock_version), locks) in grouped {
-        //For every lock with the same (region, lock_version), the txn_size is the same.
+        //Every lock with the same (region, lock_version), the txn_size is the same.
         let is_large_txn = locks.get(0).unwrap().txn_size >= 16;
 
         let commit_version =
             requests::new_cleanup_request(locks.get(0).unwrap().primary_key.clone(), lock_version)
                 .execute(pd_client.clone())
                 .await?;
+
         let request_keys = if is_large_txn {
             vec![]
         } else {
