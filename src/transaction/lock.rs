b80dda028a0cd0efb0c86d7918bda226295931bf
Renkai <gaelookair@gmail.com>
2020-05-31 22:32:33 +0800

    deal with status when region split in resolve
    Signed-off-by: Renkai <gaelookair@gmail.com>

--- src/transaction/lock.rs
+++ src/transaction/lock.rs
@@ -89,7 +89,13 @@ async fn resolve_lock_with_retry(
     let mut backoff = RESOLVE_LOCK_BACKOFF;
 
     loop {
-        let region = pd_client.region_for_id(region.id()).await?;
+        let region = if keys.is_empty() {
+            pd_client.region_for_id(region.id()).await?
+        } else {
+            //Region id for key may change caused by region split
+            pd_client.region_for_key(keys.get(0).unwrap()).await?
+        };
+
         let context = match region.context() {
             Ok(context) => context,
             Err(e) => {
