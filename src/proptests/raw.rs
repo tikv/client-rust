a9a3dda839f3ba510f70d288ab0bae01967b7449
ekexium <ekexium@gmail.com>
2020-07-15 13:50:53 +0800

    fix proptest: batch test skips duplicated keys
    Signed-off-by: ekexium <ekexium@gmail.com>

--- src/proptests/raw.rs
+++ src/proptests/raw.rs
@@ -3,6 +3,7 @@ use super::{arb_batch, pd_addrs};
 use crate::{raw::Client, Config, KvPair, Value};
 use futures::executor::block_on;
 use proptest::{arbitrary::any, proptest};
+use std::collections::HashSet;
 
 proptest! {
     /// Test single point (put, get, delete) operations on keys.
@@ -38,18 +39,22 @@ proptest! {
     ) {
         let client = block_on(Client::new(Config::new(pd_addrs()))).unwrap();
         let keys = kvs.iter().map(|kv| kv.key()).cloned().collect::<Vec<_>>();
+        let key_set = keys.iter().collect::<HashSet<_>>();
 
-        block_on(
-            client.batch_put(kvs.clone())
-        ).unwrap();
+        // skip if there are duplicated keys
+        if keys.len() == key_set.len() {
+            block_on(
+                client.batch_put(kvs.clone())
+            ).unwrap();
 
-        let out_value = block_on(
-            client.batch_get(keys.clone())
-        ).unwrap();
-        assert_eq!(kvs, out_value);
+            let out_value = block_on(
+                client.batch_get(keys.clone())
+            ).unwrap();
+            assert_eq!(kvs, out_value);
 
-        block_on(
-            client.batch_delete(keys.clone())
-        ).unwrap();
+            block_on(
+                client.batch_delete(keys.clone())
+            ).unwrap();
+        }
     }
 }
