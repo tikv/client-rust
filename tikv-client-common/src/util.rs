e7bd4567227f2b05920acf286b6e4c938a757812
Nick Cameron <nrc@ncameron.org>
2020-10-28 11:59:19 +1300

    Move duration util fn to stats
    Signed-off-by: Nick Cameron <nrc@ncameron.org>

--- tikv-client-common/src/util.rs
+++ tikv-client-common/src/util.rs
@@ -1,7 +1,5 @@
 // Copyright 2018 TiKV Project Authors. Licensed under Apache-2.0.
 
-use std::time::Duration;
-
 #[macro_export]
 macro_rules! internal_err {
     ($e:expr) => ({
@@ -13,12 +11,4 @@ macro_rules! internal_err {
     ($f:tt, $($arg:expr),+) => ({
         internal_err!(format!($f, $($arg),+))
     });
-}
-
-/// Convert Duration to seconds.
-#[inline]
-pub fn duration_to_sec(d: Duration) -> f64 {
-    let nanos = f64::from(d.subsec_nanos());
-    // In most cases, we can't have so large Duration, so here just panic if overflow now.
-    d.as_secs() as f64 + (nanos / 1_000_000_000.0)
 }
