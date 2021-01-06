63bd70ac25facfe6c853a76eba2cc9f0328b3cb5
ekexium <ekexium@gmail.com>
2021-01-07 09:03:33 +1300

    fix: rwlock in pd cluster can cause deadlock
    Signed-off-by: ekexium <ekexium@gmail.com>

--- src/pd/retry.rs
+++ src/pd/retry.rs
@@ -54,8 +54,12 @@ macro_rules! retry {
         let stats = pd_stats($tag);
         let mut last_err = Ok(());
         for _ in 0..LEADER_CHANGE_RETRY {
-            let $cluster = &$self.cluster.read().await.0;
-            match stats.done($call.await) {
+            let res = {
+                let $cluster = &$self.cluster.read().await.0;
+                $call.await
+            };
+
+            match stats.done(res) {
                 Ok(r) => return Ok(r),
                 Err(e) => last_err = Err(e),
             }
