2dada61d385ebddfbc92ff681f73951f11fc015d
Andy Lok <andylokandy@hotmail.com>
2021-01-11 16:51:15 +0800

    Revert last two commits and improve
    Signed-off-by: Andy Lok <andylokandy@hotmail.com>

--- src/pd/retry.rs
+++ src/pd/retry.rs
@@ -54,13 +54,16 @@ macro_rules! retry {
         let stats = pd_stats($tag);
         let mut last_err = Ok(());
         for _ in 0..LEADER_CHANGE_RETRY {
-            let $cluster = &$self.cluster.read().await.0;
-            match stats.done($call.await) {
+            let res = {
+                let guard = $self.cluster.read().await;
+                let $cluster = &guard.0;
+                $call.await
+            };
+
+            match stats.done(res) {
                 Ok(r) => return Ok(r),
                 Err(e) => last_err = Err(e),
             }
-            
-            drop($cluster);
 
             let mut reconnect_count = MAX_REQUEST_COUNT;
             while let Err(e) = $self.reconnect(RECONNECT_INTERVAL_SEC).await {
