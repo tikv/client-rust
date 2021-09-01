24dc19d9ecdefe506596a9bff03089599bd9d7b0
Andy Lok <andylokandy@hotmail.com>
2021-09-01 21:12:31 +0800

    patch prom
    Signed-off-by: Andy Lok <andylokandy@hotmail.com>

--- tests/common/ctl.rs
+++ tests/common/ctl.rs
@@ -6,17 +6,16 @@ use super::pd_addrs;
 use tikv_client_common::{Error, Result};
 
 pub async fn get_region_count() -> Result<u64> {
-    // let res = reqwest::get(format!("http://{}/pd/api/v1/regions", pd_addrs()[0]))
-    //     .await
-    //     .map_err(|e| Error::StringError(e.to_string()))?;
+    let res = reqwest::get(format!("http://{}/pd/api/v1/regions", pd_addrs()[0]))
+        .await
+        .map_err(|e| Error::StringError(e.to_string()))?;
 
-    // let body = res
-    //     .text()
-    //     .await
-    //     .map_err(|e| Error::StringError(e.to_string()))?;
-    // let value: serde_json::Value = serde_json::from_str(body.as_ref()).unwrap();
-    // value["count"]
-    //     .as_u64()
-    //     .ok_or_else(|| Error::StringError("pd region count does not return an integer".to_owned()))
-    todo!()
+    let body = res
+        .text()
+        .await
+        .map_err(|e| Error::StringError(e.to_string()))?;
+    let value: serde_json::Value = serde_json::from_str(body.as_ref()).unwrap();
+    value["count"]
+        .as_u64()
+        .ok_or_else(|| Error::StringError("pd region count does not return an integer".to_owned()))
 }
