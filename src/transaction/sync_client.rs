9d2f1a1fca9deb8d875044b215c0bcc954043900
Otto Westerlund <westerlundotto@gmail.com>
2026-01-16 13:07:28 +0200

    Add comments and sign-off-by message
    Signed-off-by: Otto Westerlund <westerlundotto@gmail.com>

--- src/transaction/sync_client.rs
+++ src/transaction/sync_client.rs
@@ -14,10 +14,12 @@ pub struct SyncTransactionClient {
 }
 
 impl SyncTransactionClient {
+    /// Synchronous version of `TransactionClient::new`.
     pub fn new<S: Into<String>>(pd_endpoints: Vec<S>) -> Result<Self> {
         Self::new_with_config(pd_endpoints, Config::default())
     }
 
+    /// Synchronous version of `TransactionClient::new_with_config`.
     pub fn new_with_config<S: Into<String>>(pd_endpoints: Vec<S>, config: Config) -> Result<Self> {
         let runtime =
             Arc::new(tokio::runtime::Runtime::new().expect("Failed to create Tokio runtime"));
@@ -25,16 +27,19 @@ impl SyncTransactionClient {
         Ok(Self { client, runtime })
     }
 
+    /// Synchronous version of `TransactionClient::begin_optimistic`.
     pub fn begin_optimistic(&self) -> Result<SyncTransaction> {
         let inner = self.runtime.block_on(self.client.begin_optimistic())?;
         Ok(SyncTransaction::new(inner, Arc::clone(&self.runtime)))
     }
 
+    /// Synchronous version of `TransactionClient::begin_pessimistic`.
     pub fn begin_pessimistic(&self) -> Result<SyncTransaction> {
         let inner = self.runtime.block_on(self.client.begin_pessimistic())?;
         Ok(SyncTransaction::new(inner, Arc::clone(&self.runtime)))
     }
 
+    /// Synchronous version of `TransactionClient::begin_with_options`.
     pub fn begin_with_options(&self, options: TransactionOptions) -> Result<SyncTransaction> {
         let inner = self
             .runtime
@@ -47,14 +52,17 @@ impl SyncTransactionClient {
         SyncSnapshot::new(inner, Arc::clone(&self.runtime))
     }
 
+    /// Synchronous version of `TransactionClient::current_timestamp`.
     pub fn current_timestamp(&self) -> Result<Timestamp> {
         self.runtime.block_on(self.client.current_timestamp())
     }
 
+    /// Synchronous version of `TransactionClient::gc`.
     pub fn gc(&self, safepoint: Timestamp) -> Result<bool> {
         self.runtime.block_on(self.client.gc(safepoint))
     }
 
+    /// Synchronous version of `TransactionClient::cleanup_locks`.
     pub fn cleanup_locks(
         &self,
         range: impl Into<BoundRange>,
@@ -65,6 +73,7 @@ impl SyncTransactionClient {
             .block_on(self.client.cleanup_locks(range, safepoint, options))
     }
 
+    /// Synchronous version of `TransactionClient::unsafe_destroy_range`.
     pub fn unsafe_destroy_range(&self, range: impl Into<BoundRange>) -> Result<()> {
         self.runtime
             .block_on(self.client.unsafe_destroy_range(range))
